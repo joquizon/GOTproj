ca46d2cbb895bb594eb1aae56241a16a58da64aa
joquizon <josequizon@jcqfolio.com>
2016-11-03 05:09:46 -0400

    update

--- got/got.php
+++ got/got.php
@@ -1,17 +1,17 @@
-<?php
-
-
-
-// $homepage = file_get_contents('http://www.imdb.com/find?ref_=');
-// $info = file_get_contents('http://www.imdb.com/find?q='.$X.'&s=tt&exact=true&ref_=fn_al_tt_ex')
-// echo $homepage;
-
-
-
-
-$info = file_get_contents('http://gameofthrones.wikia.com/wiki/'.$getter);
-
-preg_match('/<span class="mw-headline" id="Biography"> Biography <\/span>(.*?)id="Seas/s',$info, $subinfo);
-echo $subinfo[0];
-
+<?php
+
+
+
+// $homepage = file_get_contents('http://www.imdb.com/find?ref_=');
+// $info = file_get_contents('http://www.imdb.com/find?q='.$X.'&s=tt&exact=true&ref_=fn_al_tt_ex')
+// echo $homepage;
+
+
+
+
+$info = file_get_contents('http://gameofthrones.wikia.com/wiki/'.$getter);
+
+preg_match('/<span class="mw-headline" id="Biography"> Biography <\/span>(.*?)id="Seas/s',$info, $subinfo);
+echo $subinfo[0];
+
 ?>
